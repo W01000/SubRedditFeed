772305282472135e2c46f145c4605956ff1562e5
W01000 <externalplay4702@gmail.com>
2023-03-20 18:52:34 -0300

    RedditFeed 0.2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React, { useState, useEffect } from "react";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import Article from "./components/Article";
 
 function App() {
-  const [articles, setArticles] = useState([]); 
+  const [articles, setArticles] = useState([]);
   const [subreddit, setSubreddit] = useState("webdev");
 
   useEffect(() => {
-    fetch(`https://www.reddit.com/r/${subreddit}/.json`) 
+    fetch(`https://www.reddit.com/r/${subreddit}/.json`)
       .then((res) => {
         if (res.status !== 200) {
           console.log("ERROR");
           return;
         }
 
-        res.json().then((data) => { 
+        res.json().then((data) => {
           if (data != null) setArticles(data.data.children);
         });
       });
   }, [subreddit]);
 
- 
   const handleChange = (e) => {
     setSubreddit(e.target.value);
   };
@@ -28,6 +28,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <FontAwesomeIcon
+          icon={faSearch}
+          style={{ color: "#000000", marginRight: "10px" }}
+        />
         <input
           type="text"
           className="input"
